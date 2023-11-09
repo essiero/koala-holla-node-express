ac66266607d2b367b847e237a622a2bd13d18b62
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2023-11-09 10:18:28 -0600

    connected server to database

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const koalaRouter = express.Router();
+const pg = require('pg');
 
 // DB CONNECTION
+let pool = new pg.Pool({
+    host: 'localhost',
+    port: 5432,
+    database: 'koala_holla'
+})
 
+pool.on('connect', () => {
+    console.log('Database connected')
+});
 
 // GET
 
