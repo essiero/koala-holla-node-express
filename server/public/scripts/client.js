30e982d518dd52869295023e7b3d717e0ba5eb28
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2023-11-09 10:40:20 -0600

    made axios get request and renderKoalas function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,8 +3,33 @@ console.log( 'js' );
 function getKoalas(){
   console.log( 'in getKoalas' );
   // axios call to server to get koalas
-  
+  axios({
+    method: 'GET',
+    url: '/koalas'
+  }).then((response) => {
+    console.log('refreshKoalas() response', response.data);
+    renderKoalas(response.data);
+  }).catch((error) => {
+    console.log('Error in GET', error);
+  })
 } // end getKoalas
+
+function renderKoalas(data){
+  const koalaTable = document.getElementById('viewKoalas')
+  koalaTable.innerHTML = '';
+
+  for(let koala of data){
+    koalaTable.innerHTML += /*html*/`
+      <tr>
+        <td>${koala.name}</td>
+        <td>${koala.age}</td>
+        <td>${koala.gender}</td>
+        <td>${koala.ready_to_transfer}</td>
+        <td>${koala.notes}</td>
+  </tr>
+    `
+  }
+}
 
 function saveKoala(){
   console.log( 'in saveKoala' );
